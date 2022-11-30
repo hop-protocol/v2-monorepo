f24d2822ba6beb077879e25ee6eedc5eff9e8df9
Miguel Mota <hello@miguelmota.com>
2022-11-30 01:57:30 -0800

    core: Add abis

---
+++ packages/v2-core/src/abi/index.ts
@@ -0,0 +1,4 @@
+export { default as connectorAbi } from './generated/Connector.json'
+export { default as feeDistributorAbi } from './generated/FeeDistributor.json'
+export { default as hubMessageBridge } from './generated/HubMessageBridge.json'
+export { default as spokeMessageBridge } from './generated/SpokeMessageBridge.json'
--- packages/v2-sdk/src/hop.ts
+++ packages/v2-sdk/src/hop.ts
@@ -1,6 +1,7 @@
 import pkg from '../package.json'
 import { EventFetcher } from './eventFetcher'
-import { providers } from 'ethers'
+import { BigNumber, providers } from 'ethers'
+import { SpokeMessageBridge__factory } from '@hop-protocol/v2-core/contracts/factories/SpokeMessageBridge__factory'
 
 export class Hop {
   eventFetcher: EventFetcher
@@ -12,5 +13,44 @@ export class Hop {
 
   get version () {
     return pkg.version
+  }
+
+  async getBundleCommittedEvents (startBlock: number, endBlock?: number): Promise<any[]> {
+    const address = ''
+    const provider = this.providers.ethereum
+    const spokeMessageBridge = SpokeMessageBridge__factory.connect(address, provider)
+    console.log(spokeMessageBridge)
+    // TODO
+    return []
+  }
+
+  watchForBunedleCommittedEvents (callback: (event: any) => void) {
+    // TODO
+  }
+
+  // relayReward = (block.timestamp - relayWindowStart) * feesCollected / relayWindow
+  async getRelayerReward (): Promise<BigNumber> {
+    // TODO
+    return BigNumber.from(0)
+  }
+
+  async getEstimatedTxCostForForwardMessage (): Promise<BigNumber> {
+    // TODO
+    return BigNumber.from(0)
+  }
+
+  async shouldAttemptForwardMessage (): Promise<boolean> {
+    // TODO
+    // const estimatedTxCost = this.getEstimatedTxCostForForwardMessage()
+    // const txOk = relayerReward > estimatedTxCost
+    // const timeOk = now > bundleCommitTime + exitTime
+    // const shouldAttempt = txOk && timeOk
+    // return shouldAttempt
+    return false
+  }
+
+  async attemptForwardMessage (): Promise<any> {
+    // TODO
+    // const tx = await contract.receiveOrForwardMessageBundle()
   }
 }
